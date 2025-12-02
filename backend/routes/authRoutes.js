985fccc7d0762bf3ab0a52cfb2a5be3a3f6594a1
ArjelAaran <arjelaaran.deocampo.cics@ust.edu.ph>
2025-12-02 10:01:47 +0800

    added Get User

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,13 +1,12 @@
-// backend/routes/authRoutes.js
 import express from 'express';
-import { registerUser, loginUser } from '../controllers/authController.js';
-
+import { registerUser, loginUser, getUser } from '../controllers/authController.js';
+import { protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
-// Functional Endpoint 1: POST /api/auth/register (CRUD: Create)
+router.get('/user', protect, getUser);
+
 router.post('/register', registerUser);
 
-// Functional Endpoint 2: POST /api/auth/login
 router.post('/login', loginUser);
 
 export default router;
