b19fe108162e5e567bd8e31837c6a3d858a5da55
ArjelAaran <arjelaaran.deocampo.cics@ust.edu.ph>
2025-12-08 15:25:14 +0800

    Update carRoutes.js

--- backend/routes/carRoutes.js
+++ backend/routes/carRoutes.js
@@ -1,8 +1,8 @@
 import express from 'express';
+import multer from 'multer'; 
+import path from 'path';     
 import { getAvailableCars, createRental, getCarDetails, getUserRentals, getRentalById } from '../controllers/carController.js';
 import { protect } from '../middleware/authMiddleware.js';
-import multer from 'multer';
-import path from 'path';
 
 const router = express.Router();
 
@@ -22,13 +22,16 @@ const fileFilter = (req, file, cb) => {
         cb(new Error('Not an image! Please upload an image.'), false);
     }
 };
+
+const upload = multer({ storage, fileFilter }); 
+
 router.get('/:carId', getCarDetails);
 
 router.get('/', getAvailableCars);
 
 router.post('/rentals', protect, upload.single('receipt'), createRental);
 
-router.get('/rentals/:userId', protect, getUserRentals); 
+router.get('/rentals/:userId', protect, getUserRentals);
 
 router.get('/rental/:id', protect, getRentalById);
 
